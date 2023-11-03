c1c071a84c3f632e6198d3fb88416713b3fec843
jczigmann <jczigmann@gmail.com>
2023-11-03 14:26:08 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -18,7 +18,8 @@ set +x
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"| tr -d '\n'`
+echo ">$NAME<"
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
